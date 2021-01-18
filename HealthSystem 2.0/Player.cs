ce92235941962488f87090332ee34f7c965ab0f6
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-01-18 14:43:19 -0400

    Initial commit

---
+++ HealthSystem 2.0/Player.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem_2._0
+{
+    class Player
+    {
+        static int health = 100;
+        static int shield = 100;
+        static int lives = 3;
+
+        public static void TakeDamage(int damage)
+        {
+            Console.WriteLine("\nYou have taken " + damage + " points of damage.\n");
+
+            int healthDamage;
+            healthDamage = damage - shield;
+
+            shield -= damage;
+
+            if (shield <= 0)
+            {
+                shield = 0;
+                health -= healthDamage;
+            }
+
+            if (health <= 0)
+            {
+                Console.WriteLine("\nYou lost a life!\n");
+                lives--;
+                health = 100;
+                shield = 100;
+            }
+            if (lives <= 0)
+            {
+                lives = 0;
+                health = 0;
+                Console.WriteLine("\n\nYou Have Died\n");
+            }
+        }
+
+        public static void Heal(int hp)
+        {
+            if(health < 100)
+            {
+                Console.WriteLine("\nYou have healed " + hp + " points.\n");
+                health += hp;
+            }
+            if (health > 100) health = 100;
+        }
+
+        public static void ShowStats()
+        {
+            Console.WriteLine("|Shield: " + Player.shield);
+            Console.WriteLine("|Health: " + Player.health);
+            Console.WriteLine("|Lives: " + Player.lives);
+        }
+
+        public static void RegenerateShield(int shieldPoints)
+        {
+            if (shield < 100)
+            {
+                Console.WriteLine("\nYou have regenerated " + shieldPoints + " points to your Shield.\n");
+                shield += shieldPoints;
+            }
+            if (shield > 100) shield = 100;
+        }
+    }
+}
---
+++ HealthSystem 2.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Health System 2.0 | Bradley Morrison\n\n");
+
+
+            Player player = new Player();
+
+            Player.ShowStats();
+
+            Player.TakeDamage(155);
+            Player.ShowStats();
+            Player.RegenerateShield(66);
+            Player.ShowStats();
+            Player.TakeDamage(70);
+            Player.ShowStats();
+            Player.Heal(66);
+            Player.ShowStats();
+            Player.Heal(200);
+            Player.ShowStats();
+            Player.RegenerateShield(200);
+            Player.ShowStats();
+            Player.TakeDamage(1000);
+            Player.ShowStats();
+
+            Console.ReadKey(true);
+        }
+    }
+}
